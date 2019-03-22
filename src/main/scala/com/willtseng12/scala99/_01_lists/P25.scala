302d9892482aec834b24d06d14ef7e048fca88f9
Will <will.tseng12@gmail.com>
2019-03-21 21:31:06 -0400

    add fisher yates shuffle to P25

--- src/main/scala/com/willtseng12/scala99/_01_lists/P25.scala
+++ src/main/scala/com/willtseng12/scala99/_01_lists/P25.scala
@@ -4,4 +4,17 @@ object P25 {
   def randomPermute[A](list: List[A]): List[A] = {
     P23.randomSelect(list.length, list)
   }
+
+  def randomPermuteFY[A](list: List[A]): List[A] = {
+    val ar = list.toBuffer
+    val rand = new util.Random
+
+    for (i <- list.length - 1 to 1 by -1) {
+      val index = rand.nextInt(i)
+      val temp = ar(index)
+      ar.update(index, ar(i))
+      ar.update(i, temp)
+    }
+    ar.toList
+  }
 }
--- src/test/scala/com/willtseng12/scala99/_01_lists/P25Spec.scala
+++ src/test/scala/com/willtseng12/scala99/_01_lists/P25Spec.scala
@@ -5,11 +5,14 @@ import org.scalatest.FunSuite
 class P25Spec extends FunSuite {
   test("empty list") {
     assert(0 == P25.randomPermute(List()).distinct.length)
+    assert(0 == P25.randomPermuteFY(List()).distinct.length)
   }
   test("singleton list") {
     assert(1 == P25.randomPermute(List(1)).distinct.length)
+    assert(1 == P25.randomPermuteFY(List(1)).distinct.length)
   }
   test("generic list") {
     assert(5 == P25.randomPermute(List(1, 2, 3, 4, 5)).distinct.length)
+    assert(5 == P25.randomPermuteFY(List(1, 2, 3, 4, 5)).distinct.length)
   }
 }
