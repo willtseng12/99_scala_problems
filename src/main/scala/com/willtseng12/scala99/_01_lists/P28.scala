2dfa028b70db1068d63cf24d7f30f97e881a1397
Will <will.tseng12@gmail.com>
2019-03-23 11:04:58 -0400

    add problem 28 part b and test

--- src/main/scala/com/willtseng12/scala99/_01_lists/P28.scala
+++ src/main/scala/com/willtseng12/scala99/_01_lists/P28.scala
@@ -5,10 +5,16 @@ object P28 {
     list.sortBy(_.length)
   }
 
-  //  def lsort[A](list: List[List[A]]): List[List[A]] = {
-  //    val freqLookup = Map[]()
-  //    for {l <- list
-  //         if freqLookup contains l.length}
-  //      freqLookup + (l.length -> freqLookup(l.length) + 1)
-  //  }
+  def lsortFreq[A](list: List[List[A]]): List[List[A]] = {
+    var freqLookup = Map[Int, Int]()
+    for (l <- list) {
+      if (freqLookup contains l.length)
+        freqLookup = freqLookup + (l.length -> (freqLookup(l.length) + 1)) else
+        freqLookup = freqLookup + (l.length -> 1)
+    }
+    //    val freqLookup = Map(P10.encode(list.map(_.length).sorted) map {
+    //      _.swap
+    //    }: _*) // unpack notation
+    list.sortBy(l => freqLookup.get(l.length))
+  }
 }
--- src/test/scala/com/willtseng12/scala99/_01_lists/P28Spec.scala
+++ src/test/scala/com/willtseng12/scala99/_01_lists/P28Spec.scala
@@ -3,8 +3,15 @@ package com.willtseng12.scala99._01_lists
 import org.scalatest.FunSuite
 
 class P28Spec extends FunSuite {
+  test("empty list") {
+    assert(Nil == P28.lsort(Nil))
+    assert(Nil == P28.lsortFreq(Nil))
+  }
+
   test("generic list") {
     assert(List(List(1), List(3, 4), List(5, 6), List(7, 8, 9)) ==
       P28.lsort(List(List(7, 8, 9), List(3, 4), List(5, 6), List(1))))
+    assert(List(List(7, 8, 9), List(1), List(3, 4), List(5, 6)) ==
+      P28.lsortFreq(List(List(7, 8, 9), List(3, 4), List(5, 6), List(1))))
   }
 }
